d844dcacc40a6a416eae03bbef7eeb2ad8d3272f
Sr-Drachen <116126364+Sr-Drachen@users.noreply.github.com>
2023-10-03 15:30:33 -0500

    veamos

--- js/script.js
+++ js/script.js
@@ -1,31 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const mostrarWebButton = document.getElementById("mostrar-web");
-  const webContent = document.getElementById("web-content");
   const stardContainer = document.querySelector(".stard");
   const space = document.querySelector(".space");
 
-  let isOn = false; // Estado del interruptor
+  // Verifica si el navegador admite el modo de pantalla completa.
+  if (document.documentElement.requestFullscreen) {
+    // Solicita el modo de pantalla completa en el elemento raíz (document.documentElement).
+    document.documentElement.requestFullscreen();
+  } else if (document.documentElement.mozRequestFullScreen) {
+    document.documentElement.mozRequestFullScreen();
+  } else if (document.documentElement.webkitRequestFullscreen) {
+    document.documentElement.webkitRequestFullscreen();
+  }
 
-  mostrarWebButton.addEventListener("click", function () {
-    if (!isOn) {
-      // Si está apagado, enciende y muestra el contenido después de 0.5 segundos
-      mostrarWebButton.style.transform = "translateY(-2em)";
-      setTimeout(function () {
-        stardContainer.style.display = "none";
-        space.style.display = "none";
-        webContent.style.display = "block"; // Muestra el contenido
-        webContent.style.opacity = "1"; // Establece la opacidad en 1 para hacerlo visible
-      }, 200);
-      isOn = true;
-      // Si está encendido, apaga y oculta el contenido
-      mostrarWebButton.style.transform = "translateY(0)";
-      stardContainer.style.display = "none";
-      space.style.display = "none";
-      webContent.style.opacity = "0"; // Cambia la opacidad a 0 para ocultar el contenido
-      setTimeout(function () {
-        webContent.style.display = "none"; // Oculta el contenido después de la transición de opacidad
-      }, 500); // Ajusta el tiempo según la duración de la transición
-      isOn = false;
-    }
-  });
+  // Asegúrate de que los elementos se oculten después de un tiempo (opcional).
+
+  // Asegúrate de que el video de fondo esté en pantalla completa.
+  if (backVideo.requestFullscreen) {
+    backVideo.requestFullscreen();
+  } else if (backVideo.mozRequestFullScreen) {
+    backVideo.mozRequestFullScreen();
+  } else if (backVideo.webkitRequestFullscreen) {
+    backVideo.webkitRequestFullscreen();
+  }
 });
